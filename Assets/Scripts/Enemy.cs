b6ddc31e66c477aecc68206d44f6011cb1c2f96a
Noah <const112@umn.edu>
2022-04-01 18:59:58 -0500

    Update gitignore

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,37 +8,35 @@ public class Enemy : MonoBehaviour
     private Waypoints Wpoints;
     private int waypointIndex;
 
-    // void Start()
-    // {
-    //     Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
-    // }
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
 
-    // void Update()
-    // {
-    //     transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
 
-    //     //the following rotates the enemy on its z axis to simulate turning
-    //     Vector3 dir = Wpoints.waypoints[waypointIndex].position - transform.position;
-    //     float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-    //     transform.rotation = Quaternion.AngleAxis(angle - 270, Vector3.forward);
+        //the following rotates the enemy on its z axis to simulate turning
+        Vector3 dir = Wpoints.waypoints[waypointIndex].position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 270, Vector3.forward);
 
-    //     if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
-    //     {
-    //         if(waypointIndex < Wpoints.waypoints.Length - 1)
-    //         {
-    //             waypointIndex++;
-    //         } else
-    //         {
-    //             Destroy(gameObject);
-    //         }
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            if(waypointIndex < Wpoints.waypoints.Length - 1)
+            {
+                waypointIndex++;
+            } else
+            {
+                Destroy(gameObject);
+            }
             
-    //     }
+        }
 
-    //     // CheckForHit();
+        // CheckForHit();
         
-    // }
-
-    
+    }
 
     private void OnTriggerEnter2D (Collider2D other)
     {
