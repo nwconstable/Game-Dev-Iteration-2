62a58e575fa1124835484dc8d148e80594ebfd69
hoomzfwp <ndamte@gmail.com>
2022-04-02 13:33:14 -0500

    re-implement enemy pathing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,41 +8,43 @@ public class Enemy : MonoBehaviour
     private Waypoints Wpoints;
     private int waypointIndex;
 
-    // void Start()
-    // {
-    //     Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
-    // }
-
-    // void Update()
-    // {
-    //     transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
-
-    //     //the following rotates the enemy on its z axis to simulate turning
-    //     Vector3 dir = Wpoints.waypoints[waypointIndex].position - transform.position;
-    //     float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-    //     transform.rotation = Quaternion.AngleAxis(angle - 270, Vector3.forward);
-
-    //     if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
-    //     {
-    //         if(waypointIndex < Wpoints.waypoints.Length - 1)
-    //         {
-    //             waypointIndex++;
-    //         } else
-    //         {
-    //             Destroy(gameObject);
-    //         }
-            
-    //     }
-
-    //     // CheckForHit();
-        
-    // }
-
-    
-
-    private void OnTriggerEnter2D (Collider2D other)
+    void Start()
     {
-        if(other.gameObject.CompareTag("Bullet")){
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        //the following rotates the enemy on its z axis to simulate turning
+        Vector3 dir = Wpoints.waypoints[waypointIndex].position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 270, Vector3.forward);
+
+        if (Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            if (waypointIndex < Wpoints.waypoints.Length - 1)
+            {
+                waypointIndex++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+         //CheckForHit();
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
             Debug.Log("Enemy hit");
             Destroy(gameObject);
             Destroy(other.gameObject);
