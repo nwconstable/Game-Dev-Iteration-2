ce97b254267d9850c0b4b72f66cbf2dcd70c161a
Noah <const112@umn.edu>
2022-04-08 14:41:18 -0500

    Land changes color for land turrets

--- Assets/Scripts/AddTurret.cs
+++ Assets/Scripts/AddTurret.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Tilemaps;
 
 public class AddTurret : MonoBehaviour
 {
     [SerializeField] private Button turretButton;
     [SerializeField] private TMP_Text buttonText;
-    [SerializeField] private Collider2D[] allowedAreas;
     private GameObject turret;
     private bool isTurretPlaced;
 
@@ -31,28 +31,40 @@ public class AddTurret : MonoBehaviour
         //Debug.Log("Waited one frame");
         GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
         string turretTag = turretToBuild.GetComponent<CustomTags>().GetAtIndex(0);
+        Debug.Log("Turret Tag: " + turretTag);
+        //Change the color of all invalid build areas to red
+        foreach (GameObject buildArea in BuildManager.instance.GetBuildAreas()) {
+            if (buildArea.GetComponent<BuildArea>().IsBuildable(turretTag)) {
+                buildArea.GetComponent<Tilemap>().color = Color.green;
+            } else {
+                buildArea.GetComponent<Tilemap>().color = Color.red;
+            }
+        }
         while(!isTurretPlaced) {
             Vector3 mousePos = Input.mousePosition;
             //Debug.Log("Mouse Position: " + mousePos);
             Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
             //Debug.Log("World Position Updated: " + worldPos);
-            // Make sure the turret is placed within the allowed areas
             GameObject[] turrets = GameObject.FindGameObjectsWithTag("Turret");
             bool isWithinAllowedArea = false;
+            //Debug.Log("Is in allowed area : " + isWithinAllowedArea);
             if(Input.GetMouseButtonDown(0)) {
-                Collider2D[] colliders = Physics2D.OverlapCircleAll(worldPos, 0.2f);
+                // This part checks whether the selected turret placement is within the allowed area for that turret
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(worldPos, 0.3f);
                 foreach(Collider2D collider in colliders) {
-                    if  (collider.gameObject.GetComponent<CustomTags>().HasTag(turretTag)) {
+                    if  (collider.gameObject.GetComponent<CustomTags>().HasTag(turretTag) && !(collider.gameObject.GetComponent<CustomTags>().HasTag("Turret"))) {
                         isWithinAllowedArea = true;
                         break;
                     }
                 }
+                // This part checks whether the turret placement is too close to another turret
                 foreach(GameObject turret in turrets) {
-                    if(Vector2.Distance(turret.transform.position, worldPos) < 0.3f) {
+                    if(Vector2.Distance(turret.transform.position, worldPos) < 0.4f) {
                         isWithinAllowedArea = false;
                         break;
                     }
                 }
+                // Places the turret
                 if(isWithinAllowedArea){
                     Vector3 newPos = new Vector3(worldPos.x, worldPos.y, 0);
                     turret = (GameObject)Instantiate(turretToBuild, newPos, transform.rotation);
@@ -64,6 +76,10 @@ public class AddTurret : MonoBehaviour
             yield return null;
         }
         //Debug.Log("Turret Placer Ended");
+        // Change th color of the build areas back to color
+        foreach (GameObject buildArea in BuildManager.instance.GetBuildAreas()) {
+            buildArea.GetComponent<Tilemap>().color = Color.white;
+        }
         buttonText.text = "Add New Turret";
     }
 }
--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -26,4 +26,32 @@ public class BuildManager : MonoBehaviour
     {
         return turretToBuild;
     }
+
+    public void SetTurretToBuild(GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+
+    // Returns a list of GameObjects that can be built on
+    public GameObject[] GetBuildAreas()
+    {
+        CustomTags[] tags = (CustomTags[]) GameObject.FindObjectsOfType(typeof(CustomTags));
+        GameObject[] buildAreas = new GameObject[tags.Length];
+        foreach(CustomTags tag in tags) {
+            int i = 0;
+            if(tag.HasTag("BuildArea")) {
+                buildAreas[i] = tag.gameObject;
+            }
+            i++;
+        }
+        // Trim the null values from the array
+        List<GameObject> buildAreasList = new List<GameObject>();
+        foreach(GameObject buildArea in buildAreas) {
+            if(buildArea != null) {
+                buildAreasList.Add(buildArea);
+            }
+        }
+        Debug.Log("Build Areas: " + buildAreasList);
+        return buildAreasList.ToArray();
+    }
 }
