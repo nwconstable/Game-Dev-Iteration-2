68dabc1551ce7f8dabfe517c62ab3d43b22aef69
hoomzfwp <ndamte@gmail.com>
2022-04-15 10:37:04 -0500

    add rudimentary level handling - up to 5 waves

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject enemyObjectToSpawn;
 
-    public float timeToSpawn;
-    private float currentTimeToSpawn;
+    public float timeToSpawn = 5f;
+    private float currentTimeToSpawn = 2f;
 
-    public int maxEnemyCount = 1000;
-    private int enemyCount;
+    private float waveCounter = 0;
+
+    public TMP_Text waveCountText;
 
     void Start()
     {
@@ -19,20 +21,36 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
-        if (currentTimeToSpawn > 0)
-        {
-            currentTimeToSpawn -= Time.deltaTime;
-        }
-        else
-        {
-            if(enemyCount < maxEnemyCount)
-            {
-                Instantiate(enemyObjectToSpawn, transform.position, transform.rotation);
-                currentTimeToSpawn = timeToSpawn;
-                enemyCount++;
-            }
-            
-        }
 
+        if (currentTimeToSpawn <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            currentTimeToSpawn = timeToSpawn;
+
+        }
+        currentTimeToSpawn -= Time.deltaTime;
+
+        waveCountText.text = "Wave: " + waveCounter;
     }
+
+    IEnumerator SpawnWave() 
+    {
+        if(waveCounter < 5)
+        {
+            waveCounter++;
+            for (int i = 0; i < waveCounter; i++)
+            {
+                SpawnEnemy();
+                yield return new WaitForSeconds(0.5f);
+            }
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyObjectToSpawn, transform.position, transform.rotation);
+    }
+
+
 }
